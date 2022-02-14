1ffc3e7acb564848415ba5d434cbd2086b1db7ae
Juan Rodriguez Villalvazo <juanrdzvzo@gmail.com>
2022-02-14 12:16:55 -0600

    subiendo archivos a github

---
+++ M03/Kata M3.py
@@ -0,0 +1,47 @@
+#Ejercicio 1
+
+a = 49
+b = 25
+
+if a >= b :
+    print("Se acerca un asteroide, CORRAN!!")
+else:
+    print ("No hay asteroide cerca")
+
+
+#Ejercicio 2
+
+a = 19
+
+if a > 20 : 
+    print ("Mira al cielo, busquemos un rayo de luz")
+
+elif a  == 20 :
+    print ("Podemos ver algun rayo de luz")
+
+else:
+    print("No hay nada que buscar!")
+
+
+
+
+#Ejercicio 3
+
+d = 26
+v = 22
+
+
+if  d > 25 and d < 1000 :
+    print("Una pieza de un asteroide impactara y ")
+if v > 25:
+    print("la velocidad del asteroide es superada, Advertencia!")
+elif v >20 and v < 25 :
+    print ("Habra un rayo de luz sobre el cielo")
+else: 
+    print("No hay peligro")
+
+
+
+
+
+
